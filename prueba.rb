0030be0c14466276b6f8826adab34f32dadacd81
Sebastian Campos <pseba@outlook.com>
2020-09-09 12:32:01 -0300

    listo el html mostrando las fotos y algunas correcciones en el codigo

--- prueba.rb
+++ prueba.rb
@@ -2,9 +2,7 @@ require 'net/http'
 require 'openssl'
 require 'json'
 
-url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=w2nVBEvhq59h1SFuEmT3BqVFXehndb3O8gMOAYhA'
-
-def get_data(url)
+def request(url)
     uri = URI(url)
     http = Net::HTTP.new(uri.host, uri.port)
     request = Net::HTTP::Get.new(uri)
@@ -15,27 +13,33 @@ def get_data(url)
 end
 
 def buid_web_page
-    html ='<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Mars Rover Photos</title>
-</head>
-<body>
-    <ul>'
-    data = get_data(url)
-    data.each do |key, value|
-        if key == "photos"
+    body = request('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=w2nVBEvhq59h1SFuEmT3BqVFXehndb3O8gMOAYhA')
+
+    html ="<!DOCTYPE html>
+    <html>
+    <head>
+        <title>Mars Rover Photos</title>
+    </head>
+    <body>
+        <ul> \n"
+
+    body.each do |arr, value|
+        if arr == "photos"
             value.each do |photos|
                 photos.each do |key, value|
-                    html += "<li><img src='#{value}'></li>" if key == "img_src"
-                    # puts value if key == "img_src"
+                    if key == "img_src"
+                        html += "<li>\n"
+                        html += "<img src='#{value}'>\n"
+                        html += "</li>\n"
+                    end
                 end
             end
         end
     end
-    foot ='</ul>
+
+    foot ="</ul>
 </body>
-</html>'
+</html>"
 
     return html + foot
 end
