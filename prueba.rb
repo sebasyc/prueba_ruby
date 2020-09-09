e4b7c9646833d3846680378fc1d6eff6443f212a
Sebastian Campos <pseba@outlook.com>
2020-09-09 00:45:01 -0300

    prueba recorrido hash

--- prueba.rb
+++ prueba.rb
@@ -4,7 +4,7 @@ require 'json'
 
 url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=w2nVBEvhq59h1SFuEmT3BqVFXehndb3O8gMOAYhA'
 
-def get_data(url)
+def request(url)
     uri = URI(url)
     http = Net::HTTP.new(uri.host, uri.port)
     request = Net::HTTP::Get.new(uri)
@@ -14,4 +14,18 @@ def get_data(url)
     JSON.parse(response.read_body)
 end
 
-data = get_data(url)
+data = request(url)
+
+def buid_web_page
+    
+end
+ 
+data.each do |key, value|
+    if key == "photos"
+        value.each do |photos|
+            photos.each do |key, value|
+                puts "la url de la foto es #{value}" if key == "img_src"
+            end
+        end
+    end
+end
